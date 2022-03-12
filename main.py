9a291d474a13d6948c144d3b05764d73d399990d
ETSKA_ <kallioineneetu@gmail.com>
2022-03-12 17:22:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,121 @@
+import ScreenCloud
+import json
+import traceback
+import urllib.request, urllib.error, urllib.parse
+
+from PythonQt.QtCore import QSettings, QByteArray, QBuffer, QIODevice, QFile
+from PythonQt.QtGui import QWidget, QDialog
+from PythonQt.QtUiTools import QUiLoader
+
+class TokenUploader():
+
+    def __init__(self):
+
+        self.uil = QUiLoader()
+        self.loadSettings()
+
+    def showSettingsUI(self, parentWidget):
+
+        self.parentWidget = parentWidget
+        self.settingsDialog = self.uil.load(QFile(workingDir + "/settings.ui"), parentWidget)
+
+        self.settingsDialog.group_screenshot.input_name.connect("textChanged(QString)", self.nameFormatEdited)
+        self.settingsDialog.connect("accepted()", self.saveSettings)
+        self.loadSettings()
+
+        self.settingsDialog.group_url.input_token.text = self.url_token
+        self.settingsDialog.group_url.input_address.text = self.url_address
+        self.settingsDialog.group_screenshot.input_name.text = self.name_format
+
+        self.settingsDialog.open()
+
+    def nameFormatEdited(self, name_format):
+
+        self.settingsDialog.group_screenshot.label_example.setText(ScreenCloud.formatFilename(name_format))
+
+    def loadSettings(self):
+
+        settings = QSettings()
+
+        settings.beginGroup("uploaders")
+        settings.beginGroup("tokenupload")
+
+        self.url_token = settings.value("url-token", "")
+        self.url_address = settings.value("url-address", "")
+        self.name_format = settings.value("name-format", "Screenshot (%Y-%m-%d %H-%M-%S)")
+
+        settings.endGroup()
+        settings.endGroup()
+
+    def saveSettings(self):
+
+        settings = QSettings()
+
+        settings.beginGroup("uploaders")
+        settings.beginGroup("tokenupload")
+
+        settings.setValue("url-token", self.settingsDialog.group_url.input_token.text)
+        settings.setValue("url-address", self.settingsDialog.group_url.input_address.text)
+        settings.setValue("name-format", self.settingsDialog.group_screenshot.input_name.text)
+
+        settings.endGroup()
+        settings.endGroup()
+
+    def isConfigured(self):
+
+        self.loadSettings()
+        return not(not self.url_token or not self.url_address)
+
+    def getFilename(self):
+
+        self.loadSettings()
+        return ScreenCloud.formatFilename(self.name_format)
+
+    def upload(self, screenshot, name):
+
+        self.loadSettings()
+
+        q_ba = QByteArray()
+        q_buff = QBuffer(q_ba)
+
+        q_buff.open(QIODevice.WriteOnly)
+        screenshot.save(q_buff, ScreenCloud.getScreenshotFormat())
+        q_buff.close()
+
+        image = q_ba.toBase64().data()
+        data = urllib.parse.urlencode({'token': self.url_token, 'name': name, 'image': image.decode('utf-8')})
+        binary = data.encode('utf-8')
+
+        try:
+
+            reply = urllib.request.urlopen(self.url_address, binary)
+
+            response = reply.read().decode('utf-8')
+            data = json.loads(response)
+            error = data.get('error')
+            url = data.get('href')
+
+            if error:
+
+                raise Exception(error)
+
+            ScreenCloud.setUrl(url)
+
+        except urllib.error.HTTPError as e:
+
+            ScreenCloud.setError("Error while connecting to: " + self.url_address + "\nError:\n" + e.fp.read())
+            return False
+
+        except Exception as e:
+
+            try:
+
+                ScreenCloud.setError("Could not upload to: " + self.url_address + "\nError: {0}".format(str(e)))
+
+            except AttributeError:
+
+                ScreenCloud.setError("Unexpected error while uploading:\n" + traceback.format_exc())
+
+            return False
+
+        return True
