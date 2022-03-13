cf0d6137cd0428210d880900968e09bee490f639
ETSKA_ <contact@etska.fi>
2022-03-13 21:08:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -86,9 +86,12 @@ class TokenUploader():
         data = urllib.parse.urlencode({'token': self.url_token, 'name': name, 'image': image.decode('utf-8')})
         binary = data.encode('utf-8')
 
+        req = urllib.request.Request(self.url_address, binary)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36')
+        
         try:
 
-            reply = urllib.request.urlopen(self.url_address, binary)
+            reply = urllib.request.urlopen(req)
 
             response = reply.read().decode('utf-8')
             data = json.loads(response)
